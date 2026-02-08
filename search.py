70ea87bd4562d0e02046a2ddde10d39d66cb7661
Kotodama <2682064633@qq.com>
2026-02-08 21:42:49 +0800

    1

--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 ﻿from __future__ import annotations
 
 import json
-from typing import Any
+from typing import Any, Literal
 
 import anyio
 
@@ -27,25 +27,39 @@ def main(query: str, max_results: int = 1) -> dict[str, Any]:
     return {"search_result": result}
 
 
-async def async_main(query: str, max_results: int = 1) -> dict[str, Any]:
+async def async_main(
+    query: str,
+    depth: Literal["simple", "low", "medium", "high"] = "low",
+    max_results: int = 1,
+    type: Literal["json", "markdown"] = "json",
+) -> dict[str, Any]:
     """Convenience entry point for simple usage."""
 
     cfg = SearchConfig.load()
     async with AsyncSearcher(cfg) as pipeline:
-        result = json.dumps(
-            await pipeline.asearch_json(
-                query,
-                "high",  # depth: simple|low|medium|high
-                max_results=max_results,
-                chunk_target_chars=1200,
-                chunk_overlap_sentences=1,
-            ),
-            indent=2,
-            ensure_ascii=False,
-        )
+        if type == "markdown":
+            result = await pipeline.asearch_markdown(
+                query, depth, max_results=max_results
+            )
+        else:
+            result = json.dumps(
+                await pipeline.asearch_json(
+                    query,
+                    depth,  # depth: simple|low|medium|high
+                    max_results=max_results,
+                    chunk_target_chars=1200,
+                    chunk_overlap_sentences=1,
+                ),
+                indent=2,
+                ensure_ascii=False,
+            )
     return {"search_result": result}
 
 
 if __name__ == "__main__":
-    print(main("初音ミク 新曲 2025", 5)["search_result"])
-    print(anyio.run(async_main, "初音ミク 新曲 2025", 5)["search_result"])
+    # print(main("初音ミク 新曲 2025", 5)["search_result"])
+    print(
+        anyio.run(async_main, "初音ミク 新曲 2025", "low", 5, "markdown")[
+            "search_result"
+        ]
+    )
